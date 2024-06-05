e2f3bf7ebb3843d930432af7693ac9bfc6e67f8e
Darren Draper <darren.j.draper@gmail.com>
2024-06-05 23:01:09 +1000

    Building up basic structure of site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import About from './pages/About';
+import Header from './components/Header';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Container>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route 
+            path="/about"
+            element={
+              <About />
+            }
+          />
+        </Routes>
+
+      </BrowserRouter>
+      {/* <Router>
+
+      </Router> */}
+      {/* <div className="App">
+        <h1>Hello!</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div> */}
+
+    </Container>
+    
   );
 }
 
